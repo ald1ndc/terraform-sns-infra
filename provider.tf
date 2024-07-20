d29412b02f59b1b27e057602321772c8efd100a9
ald1ndc <aldindc@yahoo.com>
2024-07-20 10:24:21 +0800

    updated provider tf again

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.58"
     }
   }
 }
 provider "aws" {
   region = "ap-southeast-1"
-  version = "~> 5.52.0"
+  version = "~> 5.58"
 }
